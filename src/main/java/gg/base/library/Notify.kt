3265e67cb1f67b8196a44e687520162b2766e32b
guilin <973635949@qq.com>
2020-11-19 14:31:38 +0800

    典型客户

---
+++ src/main/java/gg/base/library/Notify.kt
@@ -0,0 +1,12 @@
+package gg.base.library
+
+import androidx.databinding.BaseObservable
+
+/**
+ * Created by sss on 11/19/20 10:40.
+ * email jkjkjk.com
+ */
+@Target(AnnotationTarget.PROPERTY_SETTER)
+annotation class Notify(val id: Int){
+
+}
